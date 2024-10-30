e523d6a2c60d0e56caf8ae0bde879f31d05a37a0
d-rocha <dr.oliveira88@gmail.com>
2024-10-30 17:50:20 -0300

    :recycle: [REFACTOR] Create server file and Mock data API

---
+++ formacao-php/portifolio/data/data.php
@@ -0,0 +1,51 @@
+<?php 
+    $owner = [
+        "name" => "Davi Rocha",
+        "description" => "Esse é meu primeiro projeto em PHP aqui na formação de PHP pela Rocketseat. Trabalho com tecnologia desde 2018, desenvolvendo e-commerces apoiados na plataforma Adobe Commerce (Magento2), mas focado em JavaScript e seus frameworks(K.O.js, jQuery e React.JS). É simples, mas foi feito com muito carinho ❤️❤️❤️",
+        "avatar" => "https://avatars.githubusercontent.com/u/21270006?v=4",
+        "links" => [
+            "github" => "https://github.com/d-rocha/",
+            "linkedIn" => "https://www.linkedin.com/in/davirochaoliveira/",
+            "x" => "https://twitter.com/d__rocha"
+        ]
+    ];
+
+    $projects = [
+        [
+            "title" => "Me encontre",
+            "techs" => ["NextJS", "JavaScript", "Styled Component", "Tailwind"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2024",
+        ],
+        [
+            "title" => "Minha lojinha",
+            "techs" => ["PHP", "JavaScript", "TypeScript", "Tailwind"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2024",
+        ],
+        [
+            "title" => "Tem Tudo",
+            "techs" => ["NodeJS", "NextJS", "JavaScript", "Styled Component", "Tailwind"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2023",
+        ],
+        [
+            "title" => "Cabo Frio Experience",
+            "techs" => ["PHP", "JavaScript", "CSS"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2023",
+        ],
+        [
+            "title" => "Pede Ai",
+            "techs" => ["PHP", "Laravel", "Vue", "NuxtJS", "JavaScript", "Tailwind"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2022",
+        ],
+        [
+            "title" => "O Novo E-commerce",
+            "techs" => ["PHP", "JavaScript", "jQuery", "K.O.js", "LESS", "CSS"],
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at metus enim. Vestibulum semper velit vel posuere commodo. Sed ac convallis mi. Nulla lacinia bibendum malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla eros eros, euismod non fermentum et, pulvinar ac metus. Sed nec lacus non nulla ullamcorper pulvinar sit amet a est. Aenean ut commodo nisl. Aenean a posuere lectus. Curabitur lacinia suscipit felis id finibus. In ornare consectetur purus, eu consequat dui bibendum ac. Mauris luctus vel risus non varius. Praesent auctor tellus id tristique ullamcorper. Nulla non congue erat, at molestie lectus. Maecenas condimentum nunc quis felis maximus, ut convallis massa consequat. Donec volutpat, tellus non ornare viverra, leo odio convallis nisl, eu lacinia turpis neque non risus. Vestibulum varius imperdiet purus, ut scelerisque neque tempus sed. Vestibulum magna quam, eleifend sed sollicitudin vel, mattis eget augue. Nulla eu imperdiet quam. Cras blandit, magna tempor ultrices commodo, dui felis varius est, convallis iaculis lacus enim sit amet purus. Aenean accumsan egestas metus, ut rutrum nisl luctus euismod. Aenean tincidunt lacus ut lectus dictum consectetur. Morbi scelerisque ante eu pulvinar sagittis.",
+            "date" => "2022",
+        ],
+    ];
+?>
