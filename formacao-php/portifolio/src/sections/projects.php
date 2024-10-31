76fa478c9693574ec55edb6ff68cbd9a36cac61c
d-rocha <dr.oliveira88@gmail.com>
2024-10-31 17:49:09 -0300

    :sparkles: Projects and card layout

--- formacao-php/portifolio/src/sections/projects.php
+++ formacao-php/portifolio/src/sections/projects.php
@@ -1,4 +1,4 @@
-<section class="mx-auto max-w-[1366px] lg:h-[682px]">
+<section class="mx-auto max-w-[1366px] lg:h-[682px] mb-36">
 	<div class="pt-[72px] text-center flex flex-col gap-2 mb-14">
 		<p class="font-inconsolata text-md leading-custom1 text-base-red">Meu trabalho</p>
 
@@ -6,7 +6,7 @@
 	</div>
 
 	<!-- TODO: Project card's -->
-	<div>
-
+	<div class="flex justify-center flex-wrap gap-6">
+		<?php include "src/components/card.php"; ?>
 	</div>
 </section>
