a01070b9e9cf22eabba13450a0178d694274fbc8
d-rocha <dr.oliveira88@gmail.com>
2024-11-01 17:34:45 -0300

    :sparkles: Finish Section project list and refactor card component

--- formacao-php/portifolio/src/components/card.php
+++ formacao-php/portifolio/src/components/card.php
@@ -1,271 +1,29 @@
-<!-- TODO: Dynamic show cards consumes MOCK DATA -->
 <div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
 	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
 		<img 
-			src="" 
-			alt=""
-			class=""
+			src="<?= $project['thumbnail']; ?>"
+			alt="<?= $project['title']; ?>"
+			class="rounded-lg"
 		/>
 	</div>
 
 	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
+		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">
+			<?= $project['title']; ?>
+		</p>
 		
 		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
+			<?= $project['description']; ?>
 		</span>
 
 		<ul class="mt-[31px] flex gap-x-2 ">
-			<!-- TODO: Dynamic show tech badges consume MOCK DATA -->
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
+			<?php foreach($project['techs'] as $name => $tech): ?>
+			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max <?= fillBadgeColorTech($tech); ?>">
 				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
+					<?= $tech; ?>
 				</span>
 			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-		</ul>
-	</div>
-</div>
-
-<div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
-	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
-		<img 
-			src="" 
-			alt=""
-			class=""
-		/>
-	</div>
-
-	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
-		
-		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
-		</span>
-
-		<ul class="mt-[31px] flex gap-x-2 ">
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-		</ul>
-	</div>
-</div>
-
-<div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
-	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
-		<img 
-			src="" 
-			alt=""
-			class=""
-		/>
-	</div>
-
-	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
-		
-		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
-		</span>
-
-		<ul class="mt-[31px] flex gap-x-2 ">
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-		</ul>
-	</div>
-</div>
-
-<div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
-	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
-		<img 
-			src="" 
-			alt=""
-			class=""
-		/>
-	</div>
-
-	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
-		
-		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
-		</span>
-
-		<ul class="mt-[31px] flex gap-x-2 ">
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-		</ul>
-	</div>
-</div>
-
-<div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
-	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
-		<img 
-			src="" 
-			alt=""
-			class=""
-		/>
-	</div>
-
-	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
-		
-		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
-		</span>
-
-		<ul class="mt-[31px] flex gap-x-2 ">
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-		</ul>
-	</div>
-</div>
-
-<div class="lg:w-[508px] lg:h-[180px] rounded-xl flex gap-x-4 p-3 bg-gray-300">
-	<div class="lg:w-[224px] lg:h-[156px] rounded-lg bg-gray-200">
-		<img 
-			src="" 
-			alt=""
-			class=""
-		/>
-	</div>
-
-	<div class="w-1/2">
-		<p class="font-asap font-bold leading-custom1 text-sm text-gray-600 mb-2">Title</p>
-		
-		<span class="font-mavenPro text-base leading-custom2 text-gray-500">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Necessitatibus harum dolorum.
-		</span>
-
-		<ul class="mt-[31px] flex gap-x-2 ">
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
-
-			<li class="flex items-center rounded-full h-[18px] py-0.5 px-2 w-max bg-base-purple ">
-				<span class="font-inconsolata font-bold text-xs leading-custom1 text-gray-200 uppercase">
-					Teste
-				</span>
-			</li>
+			<?php endforeach; ?>
 		</ul>
 	</div>
 </div>
--- formacao-php/portifolio/src/sections/projects.php
+++ formacao-php/portifolio/src/sections/projects.php
@@ -1,3 +1,29 @@
+<?php
+	function fillBadgeColorTech($tech) {
+		if (!$tech) {
+			return "";
+		}
+
+		$classes = [
+			"PHP" => "bg-tech-php",
+			"C#" => "bg-tech-csharp",
+			"NodeJS" => "bg-tech-nodejs",
+			"Laravel" => "bg-tech-laravel",
+			"NextJS" => "bg-tech-nextjs",
+			"TypeScript" => "bg-tech-typescript",
+			"JavaScript" => "bg-tech-javascript",
+			"JSX" => "bg-tech-jsx",
+			"Gatsby" => "bg-tech-gatsby",
+			"SASS" => "bg-tech-sass",
+			"CSS" => "bg-tech-css",
+			"Tailwind" => "bg-tech-tailwind",
+			"HTML" => "bg-tech-html"
+		];
+
+		return $classes[$tech] ?? "bg-base-purple";
+	}
+?>
+
 <section class="mx-auto max-w-[1366px] lg:h-[682px] mb-36">
 	<div class="pt-[72px] text-center flex flex-col gap-2 mb-14">
 		<p class="font-inconsolata text-md leading-custom1 text-base-red">Meu trabalho</p>
@@ -5,8 +31,9 @@
 		<p class="font-asap font-bold text-lg leading-custom1 text-gray-600">Veja os projetos em destaque</p>
 	</div>
 
-	<!-- TODO: Project card's -->
 	<div class="flex justify-center flex-wrap gap-6">
-		<?php include "src/components/card.php"; ?>
+		<?php foreach($projects as $project): ?>
+			<?php include "src/components/card.php"; ?>
+		<?php endforeach;?>
 	</div>
 </section>
