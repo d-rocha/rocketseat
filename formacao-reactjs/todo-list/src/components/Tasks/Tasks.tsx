c08245fb7dd4a4eab777bf403f9bac3e45e55a01
d-rocha <dr.oliveira88@gmail.com>
2024-11-25 19:25:43 -0300

    :sparkles: Create interface and add state control to create new and list tasks

--- formacao-reactjs/todo-list/src/components/Tasks/Tasks.tsx
+++ formacao-reactjs/todo-list/src/components/Tasks/Tasks.tsx
@@ -1,14 +1,44 @@
+import { useState } from 'react'
+
 import { NewItem } from './NewItem/NewItem'
-import { List } from './List/List';
+import { List } from './List/List'
 
 import styles from './Tasks.module.css'
 
+export interface ITask {
+    id: number,
+    content: string,
+    isChecked: boolean
+}
+
 export function Tasks() {
+    const [tasks, setTasks] = useState<ITask[]>([])
+    const [newTask, setNewTask] = useState('')
+
+    const handleNewItem = () => {
+        if(!newTask) return
+
+        const item: ITask = {
+            id: new Date().getTime(),
+            content: newTask,
+            isChecked: false
+        }
+
+        setTasks(state => [...state, item])
+        setNewTask('')
+    }
+
     return (
         <main className={styles.tasksWrapper}>
-            <NewItem />
+            <NewItem  
+                inputValue={newTask}
+                changeEvent={evt => setNewTask(evt.target.value)}
+                clickEvent={handleNewItem}
+            />
 
-            <List />
+           <List 
+                items={tasks}
+            />
         </main>
     );
 }
