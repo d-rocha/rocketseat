e796708493511fb8cf68912a0f0b62b3efbd7ef0
d-rocha <dr.oliveira88@gmail.com>
2024-12-05 08:55:18 -0300

    :sparkles: Add search component

---
+++ formacao-php/bookwise/src/components/search.php
@@ -0,0 +1,13 @@
+<form class="my-6 flex center justify-center gap-4">
+    <input 
+        id="search" 
+        type="search" 
+        name="search"
+        class="bg-transparent border border-white rounded w-1/3 py-[5px] px-[15px] text-base"
+        placeholder="Digite o nome do livro desejado ..."
+    />
+
+    <button type="submit">
+        <span>🔎</span>
+    </button>
+</form>
