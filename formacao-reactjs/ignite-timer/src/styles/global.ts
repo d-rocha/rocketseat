05f1f0c0ff9121ff197b7d87cc3697f6540f5140
d-rocha <dr.oliveira88@gmail.com>
2024-12-26 08:49:53 -0300

    :tada: Initial commit

---
+++ formacao-reactjs/ignite-timer/src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, ul {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: none;
+  }
+
+  body {
+    background: ${props => props.theme["gray-900"]};
+    color: ${props => props.theme["gray-300"]};
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+ 
