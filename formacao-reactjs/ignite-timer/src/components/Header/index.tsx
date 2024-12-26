cf5316e9d86a0cf4340cae38b7a13b8db291ec11
d-rocha <dr.oliveira88@gmail.com>
2024-12-26 08:51:44 -0300

    :sparkles: Add header component

---
+++ formacao-reactjs/ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import { HeaderContainer, NavContainer } from "./styles";
+
+import logo from '../../assets/ignite-logo.svg';
+import icTimer from '../../assets/ic-timer.svg';
+import icHistory from '../../assets/ic-history.svg';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <NavLink to="/" title="Logo">
+       <img src={logo} alt="Imagem de dois triângulos sobrepostos" />      
+      </NavLink>
+
+      <NavContainer>
+        <NavLink to="/" title="Timer">
+          <img src={icTimer} alt="Imagem de um relogio" />
+        </NavLink>
+
+        <NavLink to="/history"title="Histórico">
+          <img src={icHistory} alt="Imagem de um papel que simboliza registro" />
+        </NavLink>
+      </NavContainer>
+    </HeaderContainer>
+  )
+}
---
+++ formacao-reactjs/ignite-timer/src/components/Header/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > img {
+      cursor: pointer;
+    }
+`;
+
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    height: 3rem;
+    border-top: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+
+    &:hover {
+      border-bottom: 3px solid ${props => props.theme["green-500"]};
+    }
+
+    &.active {
+      img {
+        filter: ${props => props.theme["filter-green"]};
+      }
+    }
+  }
+`;
