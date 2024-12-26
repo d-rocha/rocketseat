9e2932790ea3329bb5cd8a9632acc344480f4d0b
d-rocha <dr.oliveira88@gmail.com>
2024-12-26 08:55:00 -0300

    :sparkles: Add home page

---
+++ formacao-reactjs/ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { NewTask } from "../../components/Task/NewTask";
+
+import { HomeContainer } from "./styles";
+
+export function Home(){
+  return (
+    <HomeContainer>
+      <NewTask />
+    </HomeContainer>
+  )
+}
---
+++ formacao-reactjs/ignite-timer/src/pages/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 3.5rem;
+  margin: 4.625rem auto;
+`;
